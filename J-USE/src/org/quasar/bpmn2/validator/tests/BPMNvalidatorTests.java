9bce6e127b850c32529f01d7c6c42e51067c8968
fernando.brito.abreu@gmail.com <fernando.brito.abreu@gmail.com@39005fe6-ade9-6191-8b48-12b6a7da1039>
2012-06-28 15:24:53 +0000

    BPMN validator test suite bootstrap

--- J-USE/src/org/quasar/bpmn2/validator/tests/BPMNvalidatorTests.java
+++ J-USE/src/org/quasar/bpmn2/validator/tests/BPMNvalidatorTests.java
@@ -4,6 +4,12 @@
  ***********************************************************/
 package org.quasar.bpmn2.validator.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -23,8 +29,8 @@ public class BPMNvalidatorTests
 
 	private final static String				MODEL_DIRECTORY		= "D:/Dropbox/Anacleto/Metamodels/BPMN2";
 	private final static String				MODEL_FILE			= "BPMN2.0n.use";
-	private final static String				SOIL_DIRECTORY		= "TestSuiteModels/";
-
+	private final static String				SOIL_DIRECTORY		= "TestSuiteModels";
+	
 	private static JUSE_ProgramingFacade	api					= new JUSEfacadeImplementation();
 
 	/***********************************************************
@@ -66,53 +72,106 @@ public class BPMNvalidatorTests
 	@After
 	public void tearDown() throws Exception
 	{
-		System.out.println("\t# objects = " + api.allObjects().size() + "\t\t# links = " + api.allLinks().size() + "\n");
-
-		System.out.println("\nINVARIANTS:");
+		// System.out.println("\t# objects = " + api.allObjects().size() + "\t\t# links = " + api.allLinks().size() + "\n");
+		if (getFailedInvariants().size()>1)
+			for (MClassInvariant anInvariant : getFailedInvariants())
+				printInvariantInfo(anInvariant);
+	}
+	
+	/***********************************************************
+	* @return
+	***********************************************************/
+	private Set<MClassInvariant> getFailedInvariants()
+	{
+		Set<MClassInvariant> result = new HashSet<MClassInvariant>();
 		for (MClassInvariant anInvariant : api.allInvariants())
 		{
 			if (!api.check(anInvariant))
-			{
-				System.out.print("\tchecking invariant '" + anInvariant.cls().name() + "::" + anInvariant.name() + "' : ");
-				if (anInvariant.getAllAnnotations().keySet().isEmpty())
-					System.out.println("FAIL [Message missing ...]");
-				else
-					for (String key : anInvariant.getAllAnnotations().keySet())
-					{
-						switch (anInvariant.getAnnotationValue(key, "type").charAt(0))
-						{
-							case 'S':
-								System.out.print("[BPMN2 specification non-conformance]");
-								break;
-							case 'W':
-								System.out.print("[Warning Message]");
-								break;
-							case 'I':
-								System.out.print("[Information Message]");
-								break;
-							case 'P':
-								System.out.print("[Plain Message]");
-								break;
-							default:
-								System.out.print("UNKNOWN MESSAGE TYPE!!!");
-						}
-						System.out.print("> " + anInvariant.getAnnotationValue(key, "msg"));
-						System.out.println(" (" + anInvariant.getAnnotationValue(key, "origin") + ")");
-					}
-			}
+				result.add(anInvariant);
 		}
-	}
-
-	@Test
-	public void aCollaborationCanOnlyHaveOneInplicitProcess_TRUE()
-	{
-		api.readSOIL(SOIL_DIRECTORY + "aCollaborationCanOnlyHaveOneInplicitProcess_TRUE.cmd", true);
+		return result;
 	}
 	
-	@Test
-	public void aCollaborationCanOnlyHaveOneInplicitProcess_FALSE()
+	/***********************************************************
+	* @param invariantName
+	* @return
+	***********************************************************/
+	private boolean invariantFails(String invariantName)
 	{
-		api.readSOIL(SOIL_DIRECTORY + "aCollaborationCanOnlyHaveOneInplicitProcess_FALSE.cmd", true);
+		for (MClassInvariant anInvariant : getFailedInvariants())
+		{
+			// System.out.println("INVARIANT NAME>" + anInvariant.name() + "<");
+			if (anInvariant.name().equals(invariantName))
+				return true;
+		}
+		return false;
+	}
+	
+	/***********************************************************
+	* @param anInvariant
+	***********************************************************/
+	private void printInvariantInfo(MClassInvariant anInvariant)
+	{
+		System.out.print("\tchecking invariant '" + anInvariant.cls().name() + "::" + anInvariant.name() + "' : ");
+		if (anInvariant.getAllAnnotations().keySet().isEmpty())
+			System.out.println("FAIL [Message missing ...]");
+		else
+			for (String key : anInvariant.getAllAnnotations().keySet())
+			{
+				switch (anInvariant.getAnnotationValue(key, "type").charAt(0))
+				{
+					case 'S':
+						System.out.print("[BPMN2 specification non-conformance]");
+						break;
+					case 'B':
+						System.out.print("[Best practice violation]");
+						break;
+					case 'I':
+						System.out.print("[Information Message]");
+						break;
+					default:
+						System.out.print("UNKNOWN MESSAGE TYPE!!!");
+				}
+				System.out.print("> " + anInvariant.getAnnotationValue(key, "msg"));
+				System.out.println(" (" + anInvariant.getAnnotationValue(key, "origin") + ")");
+			}
 	}
 
+	/***********************************************************
+	* 
+	***********************************************************/
+	private void testStub()
+	{
+		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+		String invariantName = stack[2].getMethodName();
+		
+//		System.out.println(">>>" +invariantName );
+		
+		api.readSOIL(SOIL_DIRECTORY + "/PASS/" + invariantName + ".cmd", true);
+		assertEquals(0, getFailedInvariants().size());
+
+		api.readSOIL(SOIL_DIRECTORY + "/FAIL/" + invariantName + ".cmd", true);
+		assertEquals(1, getFailedInvariants().size());
+//		System.out.println("DEBUG>" +invariantFails(invariantName));
+		assertTrue(invariantFails(invariantName));
+	}
+	
+	/***********************************************************
+	* 
+	***********************************************************/
+	@Test
+	public void aCollaborationCanOnlyHaveOneInplicitProcess()
+	{
+		testStub();
+	}
+
+	/***********************************************************
+	* 
+	***********************************************************/
+	@Test
+	public void existsCatchSignalEvent()
+	{
+		testStub();
+	}
+	
 }
