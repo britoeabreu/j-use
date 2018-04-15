1267d552c134ddbfd2804c58f0d9d346a02a4ee8
fba <fba@QUASAR.home>
2018-04-15 10:07:41 +0100

    Project reorganization, to avoid nested directories

---
+++ src/org/quasar/m2dm/Main.java
@@ -0,0 +1,17 @@
+package org.quasar.m2dm;
+
+public class Main
+{
+
+	public Main()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+
+	}
+
+}
